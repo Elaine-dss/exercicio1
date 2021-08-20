1d6dad8053a3da96060942963673a87e8803e235
Elaine <elaine.silva@zup.com.br>
2021-08-20 15:45:06 -0300

    Adicionando os estados do Brasil

--- exercicio1.bat
+++ exercicio1.bat
@@ -42,7 +42,7 @@ md "Vila Nova"
 md "Ponte Preta"
 md Londrina
 md Vitoria
-md Confiança	
+md Confianca	
 md "Brasil de Pelotas"
 cd ..
 cd ..
@@ -100,3 +100,30 @@ md Peru
 md Suriname
 md Uruguai
 md Venezuela
+cd Brasil
+md Acre
+md Alagoas
+md Amapa
+md Amazonas
+md Bahia
+md Ceara
+md "Espirito Santo"
+md "Goias"
+md "Maranhao"
+md "Mato Grosso"
+md "Mato Grosso do Sul"
+md "Minas Gerais"
+md "Para"
+md "Paraiba"
+md "Parana"
+md "Pernambuco"
+md "Piaui"
+md "Rio de Janeiro"
+md "Rio Grande do Norte"
+md "Rio Grande do Sul"
+md "Rondonia"
+md "Roraima"
+md "Santa Catarina"
+md "Sao Paulo"
+md Sergipe
+md Tocantins
