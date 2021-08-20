9f134d44da457444e9e05592e26a17c2784a183a
Elaine <elaine.silva@zup.com.br>
2021-08-20 14:49:58 -0300

    Adicionando os times da série A

--- exercicio1.bat
+++ exercicio1.bat
@@ -1,3 +1,24 @@
 md "Todos os clubes" "Todos os Estados" "Todos os paises"
 cd "Todos os clubes"
 md "Serie A" "Serie B"
+cd "Serie A"
+md Atletico-MG
+md Palmeiras
+md Fortaleza
+md Bragantino
+md Flamengo
+md Athletico-PR
+md Atletico-GO
+md Ceara
+md Internacional
+md Santos
+md Corinthians
+md Juventude
+md Bahia
+md "Sao Paulo"
+md Fluminense
+md Cuiaba
+md Sport
+md America-MG
+md Gremio
+md Chapecoense
